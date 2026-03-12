ded8818fc0f17e1127b005f2e97a268797c28451
AiiiiDannn <xhai2@uci.edu>
2026-03-12 16:54:44 -0700

    fix: import App instead of App_2

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,10 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
-import App_2 from "./App_2.tsx";
+import App from "./App.tsx";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <App_2 />
+    <App />
   </StrictMode>,
 );
